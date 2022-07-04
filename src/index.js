4ee9707dba7f008209d1865521a045cff5a79e29
bo4onoksromom <boikov_av@mail.ru>
2022-07-04 22:27:41 +0300

    feat: add function(reverse-string)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let str=Math.abs(n).toString();
+  let i=0;
+  let res='';
+  while (i<=str.length-1) {
+    res=str[i]+res;
+    i=i+1;
+  }
+  return (res);
 }
